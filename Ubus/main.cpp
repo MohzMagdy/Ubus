f44006a68a5508edfb77e9ba4873b1ee88cfe0a7
Abdelateef Khaled <77527260+Abdelateef@users.noreply.github.com>
2023-01-06 02:58:25 +0200

    Time class implemented totally

--- Ubus/main.cpp
+++ Ubus/main.cpp
@@ -20,4 +20,26 @@ int main()
 	}
 
 	delete pComp;
+
 }
+
+
+/// this a test for time class you can test it by youself guys<3
+
+//#include"Time.h"
+//#include<iostream>
+//using namespace std;
+//int main() {
+//	Time h(1, 20); Time v; Time H(48);
+//	cout<<h.Getdays()<<endl;
+//	cout<<h.Gethours() << endl;
+//	h.PrintTime();
+//	v.Setdays(1);
+//	v.Sethours(23);
+//	v.PrintTime();
+//	H.PrintTime();
+//	h.operator+(v).PrintTime();
+//	h.operator-(v).PrintTime();
+//	h.operator+(25).PrintTime();
+//	h.operator-(4).PrintTime();
+//}
